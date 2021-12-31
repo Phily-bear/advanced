89386ec823f3abdec526096484e508d0c3959966
Rinzzz <1164699978@qq.com>
2021-12-31 22:16:35 +0800

    后台统计页面

---
+++ backend/views/site/_postCountCharts.php
@@ -0,0 +1,50 @@
+<?php
+
+use common\models\Post;
+use Hisune\EchartsPHP\ECharts;
+/* @var $year */
+
+    //$nowYear = date('Y',time());
+    $nowYear = $year;
+
+    $posts = Post::find()->select('create_time')->all();
+
+    $yearPostCount = array();
+    for ($i = 0;$i<12;$i++)
+        $yearPostCount[$i] = 0;
+    for ($i = 0;$i<count($posts);$i++){
+        if (date('Y',$posts[$i]['create_time'])== $nowYear)
+        {
+            $month = date('m',$posts[$i]['create_time']);
+            $yearPostCount[$month-1] ++ ;
+        }
+    }
+    //            var_dump($yearPostCount);
+
+    $chart = new ECharts();
+    $chart->tooltip->show = true;
+    $chart->title->text = $nowYear.'-文章统计';
+    $chart->title->left = 'center';
+    //        $chart->color = ['blue','red','yellow'];
+    $chart->legend->data[] = '篇数';
+    $chart->legend->left = 'right';
+    $chart->legend->inactiveColor = '#548047';
+
+    $chart->xAxis[] = array(
+        'type' => 'category',
+        'data' => [$nowYear.'-'.'01',$nowYear.'-'.'02',$nowYear.'-'.'03',
+            $nowYear.'-'.'04',$nowYear.'-'.'05',$nowYear.'-'.'06',
+            $nowYear.'-'.'07',$nowYear.'-'.'08',$nowYear.'-'.'09',
+            $nowYear.'-'.'10',$nowYear.'-'.'11',$nowYear.'-'.'12',]
+
+    );
+    $chart->yAxis[] = array(
+        'type' => 'value'
+    );
+    $chart->series[] = array(
+        'name' => '篇数',
+        'type' => 'line',
+        'data' => $yearPostCount
+    );
+    echo $chart->render('second'.$nowYear);
+?>
--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -63,50 +63,20 @@ $this->title = '后台统计信息';
         <br/><br/><br/><br/><br/><br/>
         <div>
             <?php
-
-            $nowYear = date('Y',time());
-
-            $posts = Post::find()->select('create_time')->all();
-
-            $yearPostCount = array();
-            for ($i = 0;$i<12;$i++)
-                $yearPostCount[$i] = 0;
-            for ($i = 0;$i<count($posts);$i++){
-                if (date('Y',$posts[$i]['create_time'])== $nowYear)
-                {
-                    $month = date('m',$posts[$i]['create_time']);
-                    $yearPostCount[$month-1] ++ ;
+                $posts = Post::find()->select("create_time")->all();
+                $years = array();
+                for ($i = 0;$i<count($posts);$i++){
+                    $year = date('Y',$posts[$i]['create_time']);
+                    $years[$year] = $year;
                 }
-            }
-//            var_dump($yearPostCount);
-
-            $chart = new ECharts();
-            $chart->tooltip->show = true;
-            $chart->title->text = $nowYear.'-文章统计';
-            $chart->title->left = 'center';
-            //        $chart->color = ['blue','red','yellow'];
-            $chart->legend->data[] = '数量';
-            $chart->legend->left = 'right';
-            $chart->legend->inactiveColor = '#548047';
-
-            $chart->xAxis[] = array(
-                'type' => 'category',
-                'data' => [$nowYear.'-'.'01',$nowYear.'-'.'02',$nowYear.'-'.'03',
-                    $nowYear.'-'.'04',$nowYear.'-'.'05',$nowYear.'-'.'06',
-                    $nowYear.'-'.'07',$nowYear.'-'.'08',$nowYear.'-'.'09',
-                    $nowYear.'-'.'10',$nowYear.'-'.'11',$nowYear.'-'.'12',]
-
-            );
-            $chart->yAxis[] = array(
-                'type' => 'value'
-            );
-            $chart->series[] = array(
-                'name' => '数量',
-                'type' => 'line',
-                'data' => $yearPostCount
-            );
-            echo $chart->render('second');
+                $years = array_reverse($years);
+//                var_dump($years);
             ?>
+            <?php foreach ($years as $year): ?>
+                <?= $this->render('_postCountCharts', [
+                    'year'=>$year,
+                ]) ?>
+            <?php endforeach;  ?>
         </div>
     </div>
 </div>
